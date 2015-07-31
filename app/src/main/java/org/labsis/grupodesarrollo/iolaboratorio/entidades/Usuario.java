5ffc6a3573ca80ba2935c27915499b344dfcd101
Diego Barrionuevo <dsbarrionuevo@hotmail.com>
2015-07-31 17:06:52 -0300

    Agrego DBHelper y Usuario.

---
+++ app/src/main/java/org/labsis/grupodesarrollo/iolaboratorio/entidades/Usuario.java
@@ -0,0 +1,45 @@
+package org.labsis.grupodesarrollo.iolaboratorio.entidades;
+
+/**
+ * Created by Diego on 31/07/2015.
+ */
+public class Usuario {
+
+    private int id;
+    private String nombre;
+    private String clave;
+
+    public Usuario(int id, String nombre, String clave) {
+        this.id = id;
+        this.nombre = nombre;
+        this.clave = clave;
+    }
+
+    public Usuario(String nombre, String clave) {
+        this(-1, nombre, clave);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+}
